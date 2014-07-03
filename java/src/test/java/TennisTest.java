2fdc722c1817d1097b0fbd83e7c6c2c46f8bca44
Anshul Gupta <anshul.gupta@flipkart.com>
2014-07-03 18:28:45 +0530

    bare sekelton

--- java/src/test/java/TennisTest.java
+++ java/src/test/java/TennisTest.java
@@ -1,16 +1,10 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 import kata.problem.TennisGame;
-import kata.problem.TennisGame1;
-import kata.problem.TennisGame2;
-import kata.problem.TennisGame3;
 
-import oops.anshul.TennisGameSolution;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -86,26 +80,9 @@ public class TennisTest {
 
     @Test
     public void checkAllScoresTennisGame1() {
-        TennisGame1 game = new TennisGame1("player1", "player2");
-        checkAllScores(game);
+        //your impmentation of game
+//        checkAllScores(game);
     }
 
-    @Test
-    public void checkAllScoresTennisGame2() {
-        TennisGame2 game = new TennisGame2("player1", "player2");
-        checkAllScores(game);
-    }
-
-    @Test
-    public void checkAllScoresTennisGame3() {
-        TennisGame3 game = new TennisGame3("player1", "player2");
-        checkAllScores(game);
-    }
-    
-    @Test
-    public void checkAllScoresTennisGame4() {
-        TennisGameSolution game = new TennisGameSolution("player1", "player2");
-        checkAllScores(game);
-    }
 
 }
